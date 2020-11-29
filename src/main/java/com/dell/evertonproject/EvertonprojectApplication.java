d599fb00b6093353437f9b16cbdb3863c82c247e
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2020-11-29 15:53:28 -0300

    Project created

---
+++ src/main/java/com/dell/evertonproject/EvertonprojectApplication.java
@@ -0,0 +1,13 @@
+package com.dell.evertonproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertonprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvertonprojectApplication.class, args);
+	}
+
+}
