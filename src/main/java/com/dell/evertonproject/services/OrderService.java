6f06d6963cd688a2709e2e5534c3bcb0d2152fdc
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2020-11-30 11:00:40 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/dell/evertonproject/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.dell.evertonproject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dell.evertonproject.entities.Order;
+import com.dell.evertonproject.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
