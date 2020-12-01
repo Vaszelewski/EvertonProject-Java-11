22dbc16e1b33f18d50736541fda8ea8c1bf47954
Everton Vaszelewski <everton.vaszelewski@gmail.com>
2020-12-01 10:49:21 -0300

    Payment, one-to-one association

--- src/main/java/com/dell/evertonproject/config/TestConfig.java
+++ src/main/java/com/dell/evertonproject/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.dell.evertonproject.entities.Category;
 import com.dell.evertonproject.entities.Order;
 import com.dell.evertonproject.entities.OrderItem;
+import com.dell.evertonproject.entities.Payment;
 import com.dell.evertonproject.entities.Product;
 import com.dell.evertonproject.entities.User;
 import com.dell.evertonproject.entities.enums.OrderStatus;
@@ -83,5 +84,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
